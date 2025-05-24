640e716581dfa58fca6ea662db4fc89db3fe4d84
Paulitacastillo <153454591+Paulitacastillo@users.noreply.github.com>
2025-05-23 21:36:45 -0400

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -44,3 +44,12 @@ document.getElementById('ir-arriba').addEventListener('click', () => {
       behavior: 'smooth'
   });
 });
+<!-- MailerLite Universal -->
+<script>
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1537624');
+</script>
+<!-- End MailerLite Universal -->
